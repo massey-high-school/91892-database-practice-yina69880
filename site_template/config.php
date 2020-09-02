a0151eb2edbdad8eaa0371cf8eae4e8bacdd41ea
yina69880 <60630442+yina69880@users.noreply.github.com>
2020-09-03 09:32:43 +1200

    config.php

---
+++ site_template/config.php
@@ -0,0 +1,10 @@
+<?php
+
+ini_set('display_errors', 'On'); //change to off when live
+
+define ("DB_NAME", "00_l2_games");
+define ("DB_USERNAME", "root");
+define ("DB_PASSWORD","");
+define ("DB_HOST", "localhost");
+
+?>
