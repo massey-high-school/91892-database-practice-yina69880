65fb8fd7248c4ea9502a9ffaaf966493552f605b
yina69880 <60630442+yina69880@users.noreply.github.com>
2020-09-08 11:40:15 +1200

    showall

--- site_template/config.php
+++ site_template/config.php
@@ -2,9 +2,9 @@
 
 ini_set('display_errors', 'On'); //change to off when live
 
-define ("DB_NAME", "00_l2_games");
-define ("DB_USERNAME", "root");
-define ("DB_PASSWORD","");
+define ("DB_NAME", "yina69880");
+define ("DB_USERNAME", "_yina69880");
+define ("DB_PASSWORD","2488945681267a7b7c53b59da7b9ccde");
 define ("DB_HOST", "localhost");
 
 ?>
---
+++ site_template/showall.php
@@ -0,0 +1,16 @@
+    <?php include("topbit.php");
+    $find_sql = "";
+?>
+
+        <div class="box main">
+            <h2>All Results</h2>
+            
+            
+            <p>Results will go here...
+            </p>
+            
+
+            
+        </div> <!-- / main -->
+        
+    <?php include("bottombit.php") ?>
