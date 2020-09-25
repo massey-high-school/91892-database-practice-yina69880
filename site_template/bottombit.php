277c9be85038024514caa4adf1982f23c9dfd1c1
yina69880 <60630442+yina69880@users.noreply.github.com>
2020-09-25 14:05:56 +1200

    search bar

--- site_template/bottombit.php
+++ site_template/bottombit.php
@@ -3,7 +3,12 @@
            <h2>Add an App |
                <a class="side" href="showall.php">Show All</a></h2>
            
-           <p>This is where the search area goes
+           <form class="searchform" method="post" action="name.dev.php" enctype="multipart/form-data">
+            
+               <input class="search" type="text" name="Developer" size="32" value="" required placeholde="App Nam / Developer Name..."/>
+               
+                 <input class="submit" type="submit" name="find_game_name" value="&#xf02;"
+            </form>
 
             
         </div> <!-- / side bar -->
